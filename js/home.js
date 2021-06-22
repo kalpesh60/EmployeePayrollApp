78d557aac820b3b7c920bece872176294d3f7949
kalpesh60 <kalpeshdhalpe60@gmail.com>
2021-06-22 23:23:46 +0530

    Ability to Delete Employee from JSON server

--- js/home.js
+++ js/home.js
@@ -63,15 +63,27 @@ const getDeptHtml = (deptList) => {
 }
 
 const remove = (node) => {
-    let empPayrollData = empPayrollList.find(empData => empData._name == node.id);
+    let empPayrollData = empPayrollList.find(empData => empData.id == node.id);
     if (!empPayrollData) return;
     const index = empPayrollList
-        .map(empData => empData._name)
-        .indexOf(empPayrollData._name);
+        .map(empData => empData.id)
+        .indexOf(empPayrollData.id);
     empPayrollList.splice(index, 1);
-    localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
-    document.querySelector(".emp-count").textContent = empPayrollList.length;
-    createInnerHtml();
+    if (site_properties.use_local_storage.match("true")) {
+        localStorage.setItem("EmployeePayrollList", JSON.stringify(empPayrollList));
+        document.querySelector(".emp-count").textContent = empPayrollList.length;
+        createInnerHtml();
+    } else {
+        const deleteURL = site_properties.server_url + empPayrollData.id.toString();
+        makeServiceCall("DELETE", deleteURL, false)
+            .then(responseText => {
+                document.querySelector(".emp-count").textContent = empPayrollList.length;
+                createInnerHtml();
+            })
+            .catch(error => {
+                console.log("DELETE error Status: " + JSON.stringify(error));
+            });
+    }
 }
 
 const update = (node) => {
